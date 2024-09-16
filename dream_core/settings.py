5108795e35236bdd2af6d7e908ab55c89f760a69
maksym_korniev <maksymkorniev88@gmail.com>
2024-09-16 17:52:39 +0300

    add getting secret key from environment

--- dream_core/settings.py
+++ dream_core/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -19,7 +19,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-zy1r40s#=y59@1uuc-e0_iconv^%y!$adj9_z1y-%!@_w@k9%5"
+SECRET_KEY = os.environ.get(
+    "DJANGO_SECRET_KET",
+    "django-insecure-f1w81a%mlyvd264r&n6+&=j&w4d#u$&o@n5#u9izo*w2o1g_b7"
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
