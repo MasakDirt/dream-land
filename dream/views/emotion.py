06c9b60da7bda2385d97227a85a96e4f9604edf2
maksym_korniev <maksymkorniev88@gmail.com>
2024-09-16 16:51:55 +0300

    add emotion view to dream views

---
+++ dream/views/emotion.py
@@ -0,0 +1,11 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+
+from dream.models import Emotion
+
+
+class EmotionCreateView(LoginRequiredMixin, CreateView):
+    model = Emotion
+    fields = "__all__"
+    success_url = reverse_lazy("dream:dream-list")
