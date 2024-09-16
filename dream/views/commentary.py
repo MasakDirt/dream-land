a49ad945b231116f46ad71f635e47bb6a1ec34a8
maksym_korniev <maksymkorniev88@gmail.com>
2024-09-16 17:16:43 +0300

    add commentary add-remove dislike to views

--- dream/views/commentary.py
+++ dream/views/commentary.py
@@ -38,3 +38,31 @@ class CommentAddRemoveLike(LoginRequiredMixin, View):
                 "dream:dream-list",
             )
         )
+
+class CommentAddRemoveDislike(LoginRequiredMixin, View):
+    def post(
+            self, request: HttpRequest,
+            user_pk: str,
+            dream_pk: str,
+            pk: str,
+    ) -> HttpResponse:
+        user = request.user
+        commentary = get_object_or_404(Commentary, pk=pk)
+        commentary_like = CommentaryLike.objects.filter(
+            owner=user,
+            commentary=commentary
+        )
+        commentary_dislike = CommentaryDislike.objects.filter(
+            owner=user,
+            commentary=commentary
+        )
+
+        if commentary_like.exists():
+            commentary_like.delete()
+
+        if commentary_dislike.exists():
+            commentary_dislike.delete()
+        else:
+            CommentaryDislike.objects.create(owner=user, commentary=commentary)
+
+        return redirect("dream:dream-detail", user_pk=user_pk, pk=dream_pk)
