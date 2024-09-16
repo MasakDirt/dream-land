3c94457e32c334a4dd5afa3d7a3140ebe72c9c90
maksym_korniev <maksymkorniev88@gmail.com>
2024-09-16 16:08:57 +0300

    add user detail url to users urls file

---
+++ users/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from users.views import (
+    UserDetailView,
+)
+
+urlpatterns = [
+    path("<int:pk>/", UserDetailView.as_view(), name="user-detail"),
+]
+
+app_name = "users"
