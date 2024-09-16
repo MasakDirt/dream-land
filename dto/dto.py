90011c384331efe4317b599124fa447d47d65921
maksym_korniev <maksymkorniev88@gmail.com>
2024-09-16 16:34:21 +0300

    add graphic dto

--- dto/dto.py
+++ dto/dto.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 from dataclasses import dataclass
 from datetime import datetime
+from typing import Callable
 
 from django.conf.global_settings import AUTH_USER_MODEL
 from django.db.models import QuerySet
@@ -35,4 +36,24 @@ class DreamListDto:
             likes=dream.likes.all(),
             dislikes=dream.dislikes.all(),
             commentaries=dream.commentaries.all(),
+        )
+
+
+@dataclass
+class GraphicDto:
+    labels: list
+    data: list
+
+    @staticmethod
+    def _get_count(query_set: QuerySet[dict]) -> list[int]:
+        return [item["count"] for item in query_set]
+
+    @classmethod
+    def get_from_query_set(
+            cls, query_set: QuerySet[dict],
+            get_labels_func: Callable
+    ) -> GraphicDto:
+        return cls(
+            get_labels_func(query_set),
+            cls._get_count(query_set)
         )
