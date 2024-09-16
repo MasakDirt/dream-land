eeb9ec0ac837c68d3eb578e839f8f0153d4cb808
maksym_korniev <maksymkorniev88@gmail.com>
2024-09-16 16:13:34 +0300

    add to users url to user create

--- users/urls.py
+++ users/urls.py
@@ -2,10 +2,16 @@ from django.urls import path
 
 from users.views import (
     UserDetailView,
+    UserCreateView,
 )
 
 urlpatterns = [
     path("<int:pk>/", UserDetailView.as_view(), name="user-detail"),
+    path(
+        "create/",
+        UserCreateView.as_view(),
+        name="user-create"
+    ),
 ]
 
 app_name = "users"
